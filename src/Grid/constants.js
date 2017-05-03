0b63820ff98a5d8c0ffcf99b3c6dc251dd55697d
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2017-05-03 11:01:32 +0530

    Use single quotes in constants

--- src/Grid/constants.js
+++ src/Grid/constants.js
@@ -1,9 +1,9 @@
-export const GLOBAL_CSS_VALUES = ["inherit", "initial", "unset"];
+export const GLOBAL_CSS_VALUES = ['inherit', 'initial', 'unset'];
 
-export const ALIGN_ITEMS_VALUES = ["start", "end", "center", "stretch"];
+export const ALIGN_ITEMS_VALUES = ['start', 'end', 'center', 'stretch'];
 export const ALIGN_CONTENT_VALUES = [
   ...ALIGN_ITEMS_VALUES,
-  "space-around",
-  "space-between",
-  "space-evenly"
+  'space-around',
+  'space-between',
+  'space-evenly',
 ];
